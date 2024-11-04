ea2f00f31a2ed48747c4da4866d47ccb32dd7c4c
meimei <155803871+maimeimie@users.noreply.github.com>
2024-11-04 18:40:25 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 500,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // หากมีการใช้งาน preload script
+    },
+  });
+
+  win.loadFile('calendar.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
